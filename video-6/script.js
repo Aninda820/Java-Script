8f4bcec4e46cc1dd1e65e3ef2ebc1a78a0ee5c9d
Mr_ ERROR <michaelshroud01@mgmail.com>
2024-07-16 15:48:13 +0530

    Operators and Expressions

---
+++ video-6/script.js
@@ -0,0 +1,53 @@
+console.log('Operator in JS')
+
+let a = 70
+let b = 10
+console.log("a + b =", (a+b))
+console.log("a - b =", (a-b))
+console.log("a * b =", (a*b))
+console.log("a / b =", (a/b))
+console.log("a ** b =", (a**b))
+console.log("a % b =", (a%b))
+
+console.log("a++ =",a++) //first print and then incriment
+console.log("a = ", a)
+console.log("++a =",++a) //first incriment and then print 
+
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+
+
+let x = 30
+let y = 20
+console.log(x += y)
+console.log(x -= y)
+console.log(x /= y)
+console.log(x *= y)
+console.log(x **= y)
+
+
+let m = 4;
+let n = 10;
+console.log("m == n", m == n) //it check the value only
+console.log("m === n", m === n) //It check the value and also change the type of that value
+console.log("m != n", m != n)
+
+
+
+
+// Logical Operartors
+let f = 5;
+let j = 7;
+console.log(f<j && f==5);
+console.log(f>j && f==5);
+
+console.log(f < j || f == 10);
+console.log(f > j || f == 5);
+
+console.log(!false)
+console.log(!true)
+
+
+
