fdb4a1cc8039a5ab13e9d795988868e53e8c0b2b
Mr_ ERROR <michaelshroud01@mgmail.com>
2024-07-16 14:51:11 +0530

    Chapter 1, Practice set

---
+++ video-5/script.js
@@ -0,0 +1,19 @@
+// Q1
+let a = "Aninda"
+let b = 8
+console.log(a+b)
+console.log(typeof (a+b))
+
+// Q2
+const items = {
+    name: "Minato",
+    role: "Hukagey",
+    age: 30
+}
+items['son'] = "Naruto"
+items['name'] = "Jirya" //update name
+
+console.log(items)
+console.log(items['name'])
+console.log(items['age'])
+console.log(items.age) //we access the value like that
