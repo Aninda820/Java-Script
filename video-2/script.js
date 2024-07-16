a95cf714a695ae60a1a7ebd03817af20031c172d
Mr_ ERROR <michaelshroud01@mgmail.com>
2024-07-16 14:22:35 +0530

    Variables in JS

---
+++ video-2/script.js
@@ -0,0 +1,20 @@
+let a = 10;
+a = 220;    //update the value of (a)
+let b = 20;
+
+console.log(a)
+console.log(b)
+let uname = "Aninda";
+console.log(a+b);
+console.log(uname)
+console.log("This is an amazing course")
+
+var x = 33;
+{
+    var x = 44;
+    console.log(x)
+}
+console.log(x)
+
+// We can redeclear var 
+// We can't redeclear let but can update 
