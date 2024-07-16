854954489ee5ebc8ff06092a399014b6db077257
Mr_ ERROR <michaelshroud01@mgmail.com>
2024-07-16 13:52:51 +0530

    Primitives and Objects in JS

---
+++ video-4/script.js
@@ -0,0 +1,28 @@
+// bb nn ss u 
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("556") + BigInt('4')
+let e = "Aninda";
+let f = Symbol('I am a symbol');
+let g
+console.log(a, b, c, d, e, f, g);
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
+
+const name1 = {
+    'aninda': true,
+    'itachi': false,
+    'madara': undefined,
+    'sisuri': 'Susano'
+
+}
+console.log(name1['aninda'])
+console.log(name1['itachi'])
+console.log(name1['madara'])
+console.log(name1['sisuri'])
