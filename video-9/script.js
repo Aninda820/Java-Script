d7a73c240b63db33a0c96a5acd2658a85d21f292
Mr_ ERROR <michaelshroud01@mgmail.com>
2024-08-20 15:29:40 +0530

    for loops in js

--- video-9/script.js
+++ video-9/script.js
@@ -16,6 +16,13 @@ let obj = {
     subham: 33,
     akatsuki: 99
 }
+
+
+// calculate oject length 
+let obj_length = Object.keys(obj).length
+console.log(obj_length)
+
+
 for (let x in obj){
     console.log(x)
     console.log(obj[x])
