0c890f3c0c5bffed3d9ddc50eb2aca7166b98372
Mr_ ERROR <michaelshroud01@mgmail.com>
2024-08-26 18:56:16 +0530

    Console objects

---
+++ video-25/script.js
@@ -0,0 +1,21 @@
+console.log(console)
+console.error("This is a error log")
+
+let a = {
+    naruto: 348,
+    itachi: 43,
+    madara: 444,
+    kakshi: 32
+}
+console.table(a)
+
+console.warn("Don't watch porn")
+
+// console.clear() // This is a clear console method
+
+console.info("This is a info method, it's kind of same like log")
+
+
+
+console.assert(5>10) // if you pass any kind of false argument or statement it returns error istade
+console.assert(5<10) //if you pass the true argument or statement then it don't through any error
