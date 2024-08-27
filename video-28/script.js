6f37e967b5b7f6ce0d0f2d72dcdc5c4b5f80960d
Mr_ ERROR <michaelshroud01@mgmail.com>
2024-08-27 20:28:49 +0530

    DOM, BOM and Window Object

---
+++ video-28/script.js
@@ -0,0 +1,9 @@
+window.console.log(window)  //window object is a global object
+
+console.log(document)
+
+console.log(document.body)
+
+// document.body.style.background = "red" // We can change the color using js
+
+// location.href = "https://google.com"
